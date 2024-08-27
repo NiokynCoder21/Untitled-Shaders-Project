4181b2c8d648d2e8b8d8a970d38ed666d7afb174
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-08-27 15:33:04 +0200

    Stuff works
    Tags work properly when player has torch and when player does not have torch

--- Untitled Shaders Game/Assets/Scripts/TorchCheck.cs
+++ Untitled Shaders Game/Assets/Scripts/TorchCheck.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class TorchCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text hasTorchText;
+    public TMP_Text notTorchText;
+
+    public bool isTouching = false;
     void Start()
     {
         
@@ -13,6 +18,54 @@ public class TorchCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isTouching == false)
+        {
+            hasTorchText.gameObject.SetActive(false);
+            notTorchText.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("HandTorch")) //if the object does have wall tag, this to ensure it is not wall running
+        {
+            hasTorchText.gameObject.SetActive(true);
+            print("litten the torch");
+            isTouching = true;
+        }
+
+        else if(isTouching != true)
+        {
+            hasTorchText.gameObject.SetActive(false);
+            notTorchText.gameObject.SetActive(true);
+            print("begone torch");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("HandTorch")) //if the object does have wall tag, this to ensure it is not wall running
+        {
+            hasTorchText.gameObject.SetActive(true);
+            isTouching = true;
+        }
+
+        else if(isTouching != true)
+        {
+            hasTorchText.gameObject.SetActive(false);
+            notTorchText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("HandTorch")) //if the object does have wall tag, this to ensure it is not wall running
+        {
+            hasTorchText.gameObject.SetActive(false);
+            notTorchText.gameObject.SetActive(false);
+            print("end it now");
+            isTouching = false;
+        }
+
     }
 }
