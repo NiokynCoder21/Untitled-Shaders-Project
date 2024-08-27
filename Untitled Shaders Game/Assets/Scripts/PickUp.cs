db2468fd9fb674c1d35a0de89521676273934604
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-08-27 17:50:17 +0200

    Created script for pick up fresh

--- Untitled Shaders Game/Assets/Scripts/PickUp.cs
+++ Untitled Shaders Game/Assets/Scripts/PickUp.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OffTorch : MonoBehaviour
+public class PickUp : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
