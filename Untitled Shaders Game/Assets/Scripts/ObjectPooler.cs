1b1b84938a7bc2ba9dbe424ee585fd0238fad63e
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-08-27 19:05:11 +0200

    Object pooler script

--- Untitled Shaders Game/Assets/Scripts/ObjectPooler.cs
+++ Untitled Shaders Game/Assets/Scripts/ObjectPooler.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ObjectPooler Instance;
+
+    public GameObject torchPrefab;  // Torch prefab
+    public int poolSize = 10;       // Size of the pool
+
+    private Queue<GameObject> torchPool;  // Pool of torches
+
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        torchPool = new Queue<GameObject>();
+
+        // Initialize pool
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject torch = Instantiate(torchPrefab);
+            torch.SetActive(false);
+            torchPool.Enqueue(torch);
+        }
+    }
+
+    // Function to retrieve a torch from the pool
+    public GameObject GetTorchFromPool()
+    {
+        if (torchPool.Count > 0)
+        {
+            GameObject torch = torchPool.Dequeue();
+            torch.SetActive(true);
+            return torch;
+        }
+        else
+        {
+            // If pool is empty, instantiate a new torch
+            GameObject torch = Instantiate(torchPrefab);
+            return torch;
+        }
+    }
+
+    // Function to return a torch back to the pool
+    public void ReturnTorchToPool(GameObject torch)
+    {
+        torch.SetActive(false);
+        torchPool.Enqueue(torch);
     }
 }
