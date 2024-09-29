98ba2139aad5f4f2f5f45a0b58db95bb01b4efde
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-09-29 17:07:49 +0200

    set up scritable object

---
+++ Untitled Shaders Game/Assets/Scripts/DialogueScriptable.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogue", menuName = "Dialogue/Dialogue Set")]
+public class DialogueScriptable : ScriptableObject
+{
+    public string speakerName;
+    public List<DialogueLine> dialogueLines;
+
+    [System.Serializable]
+    public class DialogueLine
+    {
+        public string lineText;
+        public string speakerName;
+    }
+}
